135980563eb21c56cf1d5c3fd1fe7b2e5a0eec14
Wells Fu <wells-sde>
2017-10-18 01:00:58 +0800

    update python to get data,finished

--- src/getdata.py
+++ src/getdata.py
@@ -3,12 +3,9 @@ import sys
 import requests
 
 def getpricedata(url):
-    #url="http://api.chbtc.com/data/v1/ticker?currency=eth_cny";
-    resp = requests.get(url);
-    resp.encoding = 'utf-8';
-    print resp.text;
-
-
+    resp = requests.get(url)
+    resp.encoding = 'utf-8'
+    print(resp.text)
+    
 if __name__ == "__main__":
-url="http://api.chbtc.com/data/v1/ticker?currency=eth_cny";
-getpricedata(url);
+    getpricedata('http://api.chbtc.com/data/v1/ticker?currency=eth_cny')
