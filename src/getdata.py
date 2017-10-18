0926506653a12a9558ac5afdee3603cacbcb3c71
wells-sde <hhheshui@126.com>
2017-10-18 19:56:17 +0800

    update html

--- src/getdata.py
+++ src/getdata.py
@@ -1,11 +1,19 @@
-import os
-import sys
 import requests
+import json
 
-def getpricedata(url):
+def getpricedata(url,filename=None):
     resp = requests.get(url)
     resp.encoding = 'utf-8'
     print(resp.text)
+    #write to file
+    if(filename):
+        f = open(filename,'w')
+        json.dump(resp.json(),f)
+        f.close()
     
 if __name__ == "__main__":
-    getpricedata('http://api.chbtc.com/data/v1/ticker?currency=eth_cny')
+    print('CHBTC:')
+    getpricedata('http://api.chbtc.com/data/v1/ticker?currency=btc_cny','data_chbtc.json')
+    print('\nBITFINEX:')
+    getpricedata("https://api.bitfinex.com/v1/pubticker/btcusd",'data_bitfex.json')
+    input("press <enter> to exit...")
